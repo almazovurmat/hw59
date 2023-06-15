0d5b3977997e21420926aba9446a681dedf1fb0c
Almazov Urmat <almazov.urmat@gmail.com>
2023-06-15 12:14:08 +0600

    Removed trash

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 // import Jokes from "./containers/Jokes/Jokes";
 import NewMovies from "./containers/NewMovies/NewMovies";
-import './App.css';
 
 const App = () => (
   <div className="container">
